5dbdd2da8b7fa8d3c0317f16d217588dd7654e58
namanhgithub98 <doanhnguyen260898>
2021-12-17 17:24:27 +0700

    up

---
+++ ss3_Angular/bai_tap/angular-color-picker-app/src/app/color/color.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.css']
+})
+export class ColorComponent implements OnInit {
+  color1 = "red";
+  color2 = "yellow";
+  color3 = "green";
+  color4 = "blue";
+  color5 = "black";
+  color6 = "gray";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
